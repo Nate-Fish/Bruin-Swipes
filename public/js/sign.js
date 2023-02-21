53f626bdb65b37f351e4969e1812214ad73ac829
Pirjot <apirjot@gmail.com>
2023-02-20 23:25:21 -0800

    Signup/Login Feature for Website
    	- Started writing the foundation of signing up for an account
    	- Added new sign up and login feature (signed-in case for global
    	navigation still needs to be tested)
    	- Next Step:
    		1. Finish HTML structure of sign.html
    		2. Test standard sign up/login functions with prototypes
    		3. Update backend, using the new email service to email
    		new clients at their ucla email to ensure that they are
    		UCLA students (have some form of authorization in the
    		email)
    		4. Update the MongoDB schema to take a Full Name instead
    		of username

---
+++ public/js/sign.js
@@ -0,0 +1,9 @@
+/**
+ * Sign Up / In script for users to sign up or login into their
+ * account through sign.html. Made to be robust (as little dependent
+ * to the DOM as possible).
+ * 
+ * @author Pirjot Atwal
+ */
+
+// Write script for sign up login and authentication on backend (ucla email)
